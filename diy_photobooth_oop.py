a069bf004cf1fb6b78bd902707e9f352cfe24869
ycz <yczaubhreydy@gmail.com>
2025-06-24 16:35:37 +0800

    separate class for different functions

--- diy_photobooth_oop.py
+++ diy_photobooth_oop.py
@@ -1,35 +1,45 @@
 # 1. Import the necessary classes.
 import cv2
+import numpy as np
+from PIL import Image, ImageDraw, ImageFont
+import tkinter as tk
+from tkinter import messagebox
 
 # 2. Create a class for the camera and it's functions.
 class Capture:
 
     def __init__(self, cam):
-        self.photos = []
-        self.count = []
-        self.background_color = []
-        self.border = []
-        self.frame_text = ''
-        self.font_size = 0
         self.camera = cam
 
-    def open_camera(self):
-        cam = self.camera
-        cv2.namedWindow("Photobooth")
-        while True:
-            ret, frame = cam.read()
-            if not ret:
-                print("Failed to grab frame")
-                break
-            cv2.imshow("Photobooth", frame)
-
-            if cv2.waitKey(1) & 0xFF == 27:
-                break        
-        cam.release()
+    def read_frame(self):
+        ret, frame = self.camera.read()
+        if not ret:
+            print("Failed to grab frame")
+        return ret, frame
+    
+    def release(self):
+        self.camera.release()
         cv2.destroyAllWindows()
-open_cam = Capture(cv2.VideoCapture(0))
-open_cam.open_camera()
 
 # 3. Create a class for the Layout of the photobooth.
+class LayOut(Capture):
+    def __init__(self, cam):
+        super().__init__(cam)
+        self.photos = []
+        self.count = 0
+        self.background_color = (244, 194, 194)
+        self.border = 100
+        self.frame_text = 'Our Memories'
+        self.font_file = "Lobster-Regular.ttf"
+        self.font_size = 50
+        self.font = self.load_font()
+
+    def load_font(self):
+        try:
+            return ImageFont.truetype(self.font_file, self.font_size)
+        except IOError:
+            print("Font not found. Using default.")
+            return ImageFont.load_default()
+
 
 # 4. Create a class for saving the photos.
